611592609a7bacb2e3afdfc8ef3bf8f3c3092532
Sebastian Stübner <sebi.stuebner@gmail.com>
2020-09-07 19:26:02 +0200

    added pushover notification and while loop

--- comparedate.py
+++ comparedate.py
@@ -1,36 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+from pushover import init, Client
+import time
 
 url = "https://www.gymnasiumdorfen.de/"
 page = requests.get(url=url)
 print(page)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+init("a3jgvwuu37m48qb6x3cwhmo6qm9yt4")
 
-container = soup.find_all(class_='ce-container')
-newdate = container[0].get_text().split("\n")[2].split(" ")[0]
+while(True):
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-"""
-Datum aus Datei lesen und in var speichern
-Newsdate mit altem Datum vergleichen
-1. newsdate == olddate -> nix neues
-2. newsdate > olddate -> neues post
---> print
---> newsdate speichern
-"""
+    container = soup.find_all(class_='ce-container')
+    newdate = container[0].get_text().split("\n")[2].split(" ")[0]
 
-with open("datelog.txt", mode="r") as result:
-    olddate = result.read()
+    """
+    Datum aus Datei lesen und in var speichern
+    Newsdate mit altem Datum vergleichen
+    1. newsdate == olddate -> nix neues
+    2. newsdate > olddate -> neuer post
+    --> print
+    --> newsdate speichern
+    """
 
-d_olddate = datetime.strptime(olddate, "%d.%m.%Y")
-d_newdate = datetime.strptime(newdate, "%d.%m.%Y")
+    with open("datelog.txt", mode="r") as result:
+        olddate = result.read()
 
-if d_olddate == d_newdate:
-    print("Nix neues")
-else:
-    print("Neuer Beitrag")
-    with open("datelog.txt", mode="w") as result:
-        result.write(newdate)
+    d_olddate = datetime.strptime(olddate, "%d.%m.%Y")
+    d_newdate = datetime.strptime(newdate, "%d.%m.%Y")
 
-print(newdate)
+    if d_olddate == d_newdate:
+        print("Nix neues")
+    else:
+        print("Neuer Beitrag")
+        Client("ujdgh7fot6iyoevv54bi1nuobv92kt").send_message("Neuer Post!", title="GyDo_Scrape")
+        with open("datelog.txt", mode="w") as result:
+            result.write(newdate)
+
+    print(newdate)
+    time.sleep(60 * 60)
